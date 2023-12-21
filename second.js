c1ee8d06d40cd56d36d503205388471187f5b131
unknown <anhelinapavliuk@gmail.com>
2023-12-21 13:37:47 +0100

    chapter 2

--- second.js
+++ second.js
@@ -1,14 +1,29 @@
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(path.join(__dirname, "files", "starter.txt"), "utf8");
+    console.log(data);
+     await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"));
 
-console.log('Hello...');
+    await fsPromises.writeFile(path.join(__dirname, "files", "promiseWrite.txt"), data);
+    await fsPromises.appendFile(path.join(__dirname, "files", "promiseWrite.txt"), '\n\nNice to meet you.');
+    await fsPromises.rename(path.join(__dirname, "files", "promiseWrite.txt"), path.join(__dirname, "files", "promiseComplete.txt"));
 
-fs.writeFile(path.join(__dirname, "files", "reply.txt"), 'Nice to meet you', (err) => {
+    const newData = await fsPromises.readFile(path.join(__dirname, "files", "promiseComplete.txt"), "utf8");
+    console.log(newData);
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+fileOps();
+
+
+
+/* fs.writeFile(path.join(__dirname, "files", "reply.txt"), 'Nice to meet you', (err) => {
   if (err) throw err;
   console.log('Write complete');
 
@@ -21,7 +36,7 @@ fs.writeFile(path.join(__dirname, "files", "reply.txt"), 'Nice to meet you', (er
       console.log("Rename complete");
     });
   });
-});
+}); */
 
 
 
