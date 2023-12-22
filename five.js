ece60b88f47ad73d77ca80384b30a487175f157b
unknown <anhelinapavliuk@gmail.com>
2023-12-22 14:40:22 +0100

    chapter 5

--- five.js
+++ five.js
@@ -1,14 +1,40 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+
+
+
 const logEvents = require('./logEvents');
-
 const EventEmitter = require('events');
+class Emitter extends EventEmitter {};
 
-class MyEmitter extends EventEmitter {};
+
+const myEmitter = new Emitter;
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  let path;
+
+  if(req.url === '/' || req.url ==='index.html') {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    path = path.join(__dirname, 'views', 'index.html');
+    fs.readFile(path, 'utf8', (err, data) => {
+      res.end(data);
+    });
+  }
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-const myEmitter = new MyEmitter;
 
-myEmitter.on('log', (msg) => logEvents(msg));
 
-setTimeout(() => {
-  myEmitter.emit('log', 'Log event emitted!');
-}, 2000);
+
+/* myEmitter.on('log', (msg) => logEvents(msg));
+myEmitter.emit('log', 'Log event emitted!'); */
