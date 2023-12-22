851314cfd763d97f98fc401190da335b71579603
unknown <anhelinapavliuk@gmail.com>
2023-12-22 14:53:23 +0100

    chapter 5

--- five.js
+++ five.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3500;
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
-  let path;
+ /*  let path;
 
   if(req.url === '/' || req.url ==='index.html') {
     res.statusCode = 200;
@@ -27,7 +27,43 @@ const server = http.createServer((req, res) => {
     fs.readFile(path, 'utf8', (err, data) => {
       res.end(data);
     });
+  } */
+
+  const extension = path.extname(req.url);
+
+  let contentType;
+
+  switch (extension) {
+    case ".css":
+      contentType = 'text/css';
+      break;
+    case ".json":
+      contentType = 'application/json';
+      break;
+    case ".jpg":
+      contentType = 'image/jpeg';
+      break;
+    case ".png":
+      contentType = 'image/png';
+      break;
+    case ".txt":
+      contentType = 'text/plain';
+      break;
+  
+    default:
+      contentType = 'text/html';
+      break;
   }
+
+  let filePath = 
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.sloce(-1) === '/'
+        ? path.join(__dirname, 'views', req.url)
+        : path.join(__dirname, req.url);
+  
+  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
