19c80d4ac0fcf93ca63a983bf4cbce153d5d00ef
unknown <anhelinapavliuk@gmail.com>
2023-12-22 19:45:41 +0100

    chapter 5

--- five.js
+++ five.js
@@ -13,10 +13,39 @@ class Emitter extends EventEmitter {};
 
 const myEmitter = new Emitter;
 
+myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
+
 const PORT = process.env.PORT || 3500;
+
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath, 
+      !contentType.includes('image') ? 'utf8' : ''
+    );
+    const data = contentType === 'application/json'
+      ? JSON.parse(rawData) : rawData;
+    response.writeHead(
+      filePath.incudes('404.html') ? 404 : 200, 
+      { 'Content-Type': contentType}
+    );
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data
+    );
+    
+  } catch (err) {
+    console.log(err);
+    myEmitter.emit("log", `${err.name}: ${err.message}`, "errLog.txt"); 
+    response.statusCode = 500;
+    response.end();
+  }
+}
+
+
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+  myEmitter.emit("log", `${req.url}\t${req.method}`, 'reqLog.txt'); 
 
  /*  let path;
 
@@ -64,6 +93,26 @@ const server = http.createServer((req, res) => {
   
   if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
 
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case 'old-page.html':
+        res.writeHead(301, {'Location': '/new-page.html'});
+        res.end();
+        break;
+      case 'www-page.html':
+        res.writeHead(301, {'Location': '/'});
+        res.end();
+        break;
+      default:
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        break;
+    }
+  }
+
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
@@ -72,5 +121,5 @@ server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
-/* myEmitter.on('log', (msg) => logEvents(msg));
-myEmitter.emit('log', 'Log event emitted!'); */
+
+
--- logEvents.js
+++ logEvents.js
@@ -6,7 +6,7 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 
-const logEvents = async (message) => {
+const logEvents = async (message, logName) => {
   const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
   console.log(logItem);
@@ -14,7 +14,7 @@ const logEvents = async (message) => {
     if (!fs.existsSync(path.join(__dirname, 'logs'))) {
       await fsPromises.mkdir(path.join(__dirname, 'logs'));
     }
-    await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
+    await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
   } catch (err) {
     console.log(err);
   }
