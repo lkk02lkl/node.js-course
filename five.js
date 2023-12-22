57b8a88d13e2ac0e2b983f9bcdcee70bc04b2c7b
unknown <anhelinapavliuk@gmail.com>
2023-12-22 14:21:36 +0100

    chapter 5

---
+++ five.js
@@ -0,0 +1,14 @@
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+
+const myEmitter = new MyEmitter;
+
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+  myEmitter.emit('log', 'Log event emitted!');
+}, 2000);
