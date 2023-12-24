aad592e4ca39fd436dcd14ec0c19bd8aa1dbb747
unknown <anhelinapavliuk@gmail.com>
2023-12-24 12:25:50 +0100

    chapter 6

---
+++ six.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3500;
+
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
