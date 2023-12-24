f8546f3bbdc055cbad06a9e7d0f9af74317a9111
unknown <anhelinapavliuk@gmail.com>
2023-12-24 13:13:29 +0100

    chapter 6

--- six.js
+++ six.js
@@ -17,9 +17,34 @@ app.get('/old-page(.html)?', (req, res) => {
   res.redirect(301, '/new-page.html');
 });
 
+app.get('/hello(.html)?', (req, res, next) => {
+  console.log('attempted to lead hello.html');
+  next();
+}, (req, res) => {
+  res.spend('Hello World!');
+});
+
+
 app.get('/*', (req, res) => {
   //res.sendFile('./views/index.html', { root: __dirname });
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+const one = (req, res, next) => {
+  console.log('one');
+  next();
+}
+
+const two = (req, res, next) => {
+  console.log('two');
+  next();
+}
+
+const three = (req, res, next) => {
+  console.log('three');
+  res.send('Finished');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
